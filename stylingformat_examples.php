2e55300f225f7e98ab6ae047cdb8a74f51012ea7
fahmi <fahminoh99@gmail.com>
2022-11-17 16:17:21 +0800

    UPDATE dynamic styling format

--- stylingformat_examples.php
+++ stylingformat_examples.php
@@ -4,138 +4,130 @@
     /** 
     * Contain automated Styling Format for sheets.
     */
-    function sheetstylingFormat()
-    {   
-        $spreadsheetID = $this->spreadsheet->spreadsheetId; 
+    function stylingformat_examples(){
+    $spreadsheetID = $this->spreadsheet->spreadsheetId; 
         
-        $sheet_id = "0";
+    $sheet_name = "Sheet1";
 
-        $requests = [
+    $sheet_id = "0";
 
-            new \Google\Service\Sheets\Request([
-                "mergeCells" => [
-                    "mergeType" => "MERGE_ALL",
-                    
-                    "range" => [
-                        "sheetId" => $sheet_id,
-                        "startRowIndex" => 1,
-                        "endRowIndex" => 2,
-                        "startColumnIndex" => 0,
-                        "endColumnIndex" => (sizeof($this->dataCols->all())),
-                    ],
+    $obj = [
+        "Doc. Date" => ["numberFormat" => ["type" => "DATE", "pattern" => "dd-mm-yyyy"]],
+        "Amount (MYR)" => ["numberFormat" => ["type" => "NUMBER", "pattern" => "#,##0.00"]],
+        "Quantity" => ["numberFormat" => ["type" => "NUMBER", "pattern" => "#,##0.0000"]],
+        "Unit Price" => ["numberFormat" => ["type" => "NUMBER", "pattern" => "#,##0.000000"]]
+    ];
+
+    // Retrieve the header title.
+    $retrieve = $this->service->spreadsheets_values->get($spreadsheetID, "'" . $sheet_name . "'!A3:3");
+    $header = $retrieve["values"][0];
+
+    $requests = [
+
+        //Merge Company Info
+        new \Google\Service\Sheets\Request([
+            "mergeCells" => [
+                "mergeType" => "MERGE_ALL",
+                
+                "range" => [
+                    "sheetId" => $sheet_id,
+                    "startRowIndex" => 1,
+                    "endRowIndex" => 2,
+                    "startColumnIndex" => 0,
+                    "endColumnIndex" => (sizeof($this->dataCols->all())),
                 ],
-                ]),
-
-            new \Google\Service\Sheets\Request([
-                "repeatCell" => [
-                    "range" => [
-                        "sheetId" => $sheet_id,
-                        "startRowIndex" => 0,
-                        "endRowIndex" => 3,
-                    ],
-                    "cell" => [
-                        "userEnteredFormat" => ["horizontalAlignment" => "CENTER"],
-                    ],
-                    "fields" => "userEnteredFormat.horizontalAlignment",
-                ],
+            ],
             ]),
 
-            new \Google\Service\Sheets\Request([
-                "repeatCell" => [
-                    "range" => [
-                        "sheetId" => $sheet_id,
-                        "startColumnIndex" => 0,
-                        "endColumnIndex" => 1,
-                    ],
-                    "cell" => [
-                        "userEnteredFormat" => ["horizontalAlignment" => "LEFT"],
-                    ],
-                    "fields" => "userEnteredFormat.horizontalAlignment",
+        //Allign Company Info & Column Title 
+        new \Google\Service\Sheets\Request([
+            "repeatCell" => [
+                "range" => [
+                    "sheetId" => $sheet_id,
+                    "startRowIndex" => 0,
+                    "endRowIndex" => 3,
                 ],
-            ]),
-            
-            new \Google\Service\Sheets\Request([
-                "repeatCell" => [
-                    "range" => [
-                        "sheetId" => $sheet_id,
-                        "startRowIndex" => 0,
-                        "endRowIndex" => 3,
-                    ],
-                    "cell" => [
-                        "userEnteredFormat" => [
-                            "textFormat" => [
-                                                "bold" => true,
-                                ],
-                        ]
-                    ],
-                    "fields" => "userEnteredFormat.textFormat.bold",
+                "cell" => [
+                    "userEnteredFormat" => ["horizontalAlignment" => "CENTER"],
                 ],
-            ]),
-
-            new \Google\Service\Sheets\Request([
-                "repeatCell" => [
-                    "range" => [
-                        "sheetId" => $sheet_id,
-                        "startRowIndex" => 1,
-                        "endRowIndex" => 3,
-                        "startColumnIndex"=> 0,
-                        "endColumnIndex"=> (sizeof($this->dataCols->all())),
-                    ],
-                    "cell" => [
-                        "userEnteredFormat" => [
-                            "backgroundColor"=> [
-                                "red"=> 0.7,
-                                "green"=> 0.7,
-                                "blue"=> 0.7,
-                                ]
-                            ]
-                        ],
-                    "fields" => "userEnteredFormat.backgroundColor",
+                "fields" => "userEnteredFormat.horizontalAlignment",
+            ],
+        ]),
+        
+        //Bold Text Company Info & Column Title
+        new \Google\Service\Sheets\Request([
+            "repeatCell" => [
+                "range" => [
+                    "sheetId" => $sheet_id,
+                    "startRowIndex" => 0,
+                    "endRowIndex" => 3,
                 ],
-            ]),
-
-            new \Google\Service\Sheets\Request([
-                "autoResizeDimensions" => [
-                    "dimensions" => [
-                        "dimension" => "COLUMNS",
-                        "startIndex" => 0,
+                "cell" => [
+                    "userEnteredFormat" => [
+                        "textFormat" => [
+                                            "bold" => true,
+                            ],
                     ]
-                ]
-            ]),
-
-            new \Google\Service\Sheets\Request([
-                "repeatCell" => [
-                    "range" => [
-                        "sheetId" => $sheet_id,
-                        "startRowIndex" => 1,
-                        "endRowIndex" => 2,
-                    ],
-                    "cell" => [
-                        "userEnteredFormat" => ["horizontalAlignment" => "CENTER"],
-                    ],
-                    "fields" => "userEnteredFormat.horizontalAlignment",
                 ],
-            ]),
+                "fields" => "userEnteredFormat.textFormat.bold",
+            ],
+        ]),
 
-            new \Google\Service\Sheets\Request([
-                "repeatCell" => [
-                    "range" => [
-                        "sheetId" => $sheet_id,
-                        "startColumnIndex" => 1,
-                        "endColumnIndex" => 3,
-                    ],
-                    "cell" => [
-                        "userEnteredFormat" => [
-                            "numberFormat" => [
-                            "type" => "DATE",
-                            "pattern" => "dd-mm-yyyy"
+        //Column Title and Company Info Column Background Color
+        new \Google\Service\Sheets\Request([
+            "repeatCell" => [
+                "range" => [
+                    "sheetId" => $sheet_id,
+                    "startRowIndex" => 1,
+                    "endRowIndex" => 3,
+                    "startColumnIndex"=> 0,
+                    "endColumnIndex"=> (sizeof($this->dataCols->all())),
+                ],
+                "cell" => [
+                    "userEnteredFormat" => [
+                        "backgroundColor"=> [
+                            "red"=> 0.7,
+                            "green"=> 0.7,
+                            "blue"=> 0.7,
                             ]
                         ]
                     ],
-                    "fields" => "userEnteredFormat.numberFormat",
-                ],
-            ]),
-        ];
+                "fields" => "userEnteredFormat.backgroundColor",
+            ],
+        ]),  
+
+        //Column Sizing
+        new \Google\Service\Sheets\Request([
+            "autoResizeDimensions" => [
+                "dimensions" => [
+                    "dimension" => "COLUMNS",
+                    "startIndex" => 2,
+                ]
+            ]
+        ]),
+        
+    ];
+
+    //Date & Decimal Format
+    foreach ($header as $i => $h) {
+        if (array_key_exists($h, $obj)) {
+            array_push($requests, 
+                new \Google\Service\Sheets\Request([
+                    "repeatCell" => [
+                        "range" => [
+                            "sheetId" => $sheet_id,
+                            "startColumnIndex" => $i,
+                            "endColumnIndex" => $i + 1,
+                            "startRowIndex" => 3,
+                            "endRowIndex" => 500,
+                        ],
+                        "cell" => ["userEnteredFormat" => $obj[$h]],
+                        "fields" => "userEnteredFormat.numberFormat",
+                    ],
+                ]),
+            );
+        };
+    }
         $batchUpdate = new \Google\Service\Sheets\BatchUpdateSpreadsheetRequest(["requests" => $requests]);
         $this->service->spreadsheets->batchUpdate($spreadsheetID, $batchUpdate);
 
